1a7c8df27fd302672aac032c975a58a0e443bb28
Daniel Gilbert <dgilb19@gmail.com>
2016-11-25 16:38:16 +1000

    at this point its just tiny iterations stupid thing isnt letting me add dictionaries trying to add question asking

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def webhook():
                     elif command == 'reverse':
                         new_message_text = "reversed: {}".format(text[::-1])
 
-                    elif message_text_split[0] in ['what', 'whats', "what's", 'when', 'whens', "when's"]:
+                    elif message_text in ['what', 'whats', "what's", 'when', 'whens', "when's"]:
                         new_message_text = 'I know you are asking a question but im not that smart yet!'
 
                     else:
