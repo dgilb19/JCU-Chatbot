b58696b58bdbf8d7215b9a6d225f84de9a9e7e36
Daniel Gilbert <dgilb19@gmail.com>
2016-11-25 13:19:31 +1000

    at this point its just tiny iterations at this point

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    if message_text == [greetings]:
+                    if message_text == 'hello':
                         send_message(sender_id, random.choice[greetings])
                     else:
                         send_message(sender_id, "echo: {}".format(message_text))
