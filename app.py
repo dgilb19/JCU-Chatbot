239da8461a19490ff864d290e43f0408c74011aa
Daniel Gilbert <dgilb19@gmail.com>
2016-11-25 10:05:36 +1000

    its going to be annoying to push so many times..

--- app.py
+++ app.py
@@ -80,6 +80,7 @@ def send_message(recipient_id, message_text):
         log(r.text)
 
 
+
 def log(message):  # simple wrapper for logging to stdout on heroku
     print(str(message))
     sys.stdout.flush()
@@ -88,4 +89,3 @@ def log(message):  # simple wrapper for logging to stdout on heroku
 if __name__ == '__main__':
     app.run(debug=True)
 
-test
