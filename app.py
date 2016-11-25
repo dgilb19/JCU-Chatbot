1c6c32cf427b1df8fa24c8cb674594de2b690202
Daniel Gilbert <dgilb19@gmail.com>
2016-11-25 11:35:51 +1000

    its going to be annoying to push so many times..

--- app.py
+++ app.py
@@ -40,12 +40,13 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
+
                     send_message(sender_id, random.choice[greetings])
 
-                    if data == [greetings]:
-                        send_message(sender_id, random.choice[greetings])
-                    else:
-                        send_message(sender_id, data)
+                    # if data == [greetings]:
+                    #     send_message(sender_id, random.choice[greetings])
+                    # else:
+                    #     send_message(sender_id, data)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
