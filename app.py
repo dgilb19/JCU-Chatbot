d55e9ce2ec602350e1fe097953dd39411ad1b59a
jc427727 <dgilb19@gmail.com>
2016-11-29 11:27:15 +1000

    doin things that probs wont work

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
+                    global message_text
 
                     message_words = message_text.split(' ', 1)
                     message_text_split = message_text.split
