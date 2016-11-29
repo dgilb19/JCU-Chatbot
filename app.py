cbee17457d30f9c3f55b50587055de5171ca8e77
jc427727 <dgilb19@gmail.com>
2016-11-29 12:14:55 +1000

    doin things that probs wont work

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def webhook():
                         new_message_text = "reversed: {}".format(text[::-1])
 
                     elif message_text.split(" ")[0] in asking_word_list:
-                        new_message_text = ""
+                        new_message_text = "I know you are asking a question but im not that smart yet!"
                         test_message = test_function()
 
 
