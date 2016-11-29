c67f03a8f7ee55c68c7c95c14bc9ea0ea340cd18
jc427727 <dgilb19@gmail.com>
2016-11-29 12:03:10 +1000

    doin things that probs wont work

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import random
 
 import requests
 from flask import Flask, request
+from matplotlib.delaunay.testfuncs import data
 
 app = Flask(__name__)
 
@@ -69,28 +70,8 @@ def webhook():
 
                     elif message_text.split(" ")[0] in asking_word_list:
                         new_message_text = "I know you are asking a question but im not that smart yet!"
-                        if data["object"] == "page":
+                        test_message = test_function()
 
-                            for entry in data["entry"]:
-
-                                for messaging_event_two in entry["messaging"]:
-                                    if messaging_event_two.get("message"):
-                                        sender_id = messaging_event_two["sender"]["id"]
-                                        recipient_id_two = messaging_event_two["recipient"]["id"]
-                                        message_text_two = messaging_event_two["message"]["text"]
-
-
-                                        test_message = 'test'
-
-                                        # send_message(sender_id, 'hello my good friend')
-                                    if messaging_event_two.get("delivery"):
-                                        pass
-
-                                    if messaging_event.get("option"):
-                                        pass
-
-                                    if messaging_event.get("postback"):
-                                        pass
 
 #                     elif message_text != "":
 #                         while message_text_split_length != 0:
@@ -118,6 +99,30 @@ def webhook():
                     pass
 
     return "ok", 200
+
+
+def test_function():
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+
+            for messaging_event_two in entry["messaging"]:
+                if messaging_event_two.get("message"):
+                    sender_id = messaging_event_two["sender"]["id"]
+                    recipient_id_two = messaging_event_two["recipient"]["id"]
+                    message_text_two = messaging_event_two["message"]["text"]
+
+                    return 'test'
+
+                    # send_message(sender_id, 'hello my good friend')
+                if messaging_event_two.get("delivery"):
+                    pass
+
+                if messaging_event_two.get("option"):
+                    pass
+
+                if messaging_event_two.get("postback"):
+                    pass
 
 
 def send_message(recipient_id, message_text):
