9741482c1cc4cb53777e84f22e9192bc8ed99b91
jc427727 <dgilb19@gmail.com>
2016-11-29 14:21:07 +1000

    probs dont work

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def test_function():
                     recipient_id_two = messaging_event_two["recipient"]["id"]
                     message_text_two = messaging_event_two["message"]["text"]
 
-                    send_message(sender_id, 'hello my good friend')
+                    return send_message(sender_id, 'hello my good friend')
                 if messaging_event_two.get("delivery"):
                     pass
 
