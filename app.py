da0d2a6ef47907574b9eb0dc968a441fe7b5e35d
jc427727 <dgilb19@gmail.com>
2016-11-29 14:37:49 +1000

    new attempt attempting to get rid of unnessicery things

--- app.py
+++ app.py
@@ -40,15 +40,29 @@ def webhook():
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
 
-                if messaging_event.get("message"):  # someone sent us a message
+                # if messaging_event.get("message"):  # someone sent us a message
 
-                    sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
+                recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                message_text = messaging_event["message"]["text"]  # the message's text
 
-                    new_message_text = "I don't know what you are saying! you said this: {}".format(message_text)
 
-                    send_message(sender_id, '{}'.format(new_message_text))
+
+
+
+
+                new_message_text = "I don't know what you are saying! you said this: {}".format(message_text)
+
+                send_message(sender_id, '{}'.format(new_message_text))
+
+
+
+
+
+
+
+
+
 
                 # if messaging_event.get("delivery"):  # delivery confirmation
                 #     pass
@@ -58,6 +72,8 @@ def webhook():
                 #
                 # if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
                 #     pass
+                    # going to leave this here in case i need it!
+
 
     return "ok", 200
 
