134f44ddc3d7ed763cbd6a9db0514d83abf276bd
jc427727 <dgilb19@gmail.com>
2016-11-30 11:54:28 +1000

    TRYING TO UNBREAK IT attempt 3 reverting to an earlier version

--- app.py
+++ app.py
@@ -25,9 +25,13 @@ def verify():
 
 @app.route('/', methods=['POST'])
 def webhook():
-    # endpoint for processing incoming messaging events
-
+    greetings = ['hi', 'hello', 'hey']
+    # reversed_word_list = ['reversed', 'reverse', 'backwards']
+    # asking_word_list = ['what', 'whats', "what's", 'when', 'whens', "when's"]
+    #
     ai_greetings_word_list = ["Hi", "Hello", "Howdy", "Sup my dude"]
+
+    # endpoint for processing incoming messaging events
 
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
@@ -41,45 +45,41 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    # message_text = messaging_event["message"]["text"]  # the message's text
-                    send_message(sender_id, "please work")
-                    # if re.match(r'.*hello|hey|hi(?!reverse|reversed|backwards)', message_text, re.I):
-                    #     send_message(sender_id, "{}, how can I help you today?".format(random.choice(ai_greetings_word_list)))
-                    #
-                    # elif re.match(r'.*what|when|date|who|where', message_text, re.I):
-                    #     if re.match(r'.*what', message_text, re.I):
-                    #         question_message_text = "I know you are asking a question but I'm not that smart yet! :what"
-                    #
-                    #     elif re.match(r'.*when|date', message_text, re.I):
-                    #         question_message_text = "I know you are asking when something is, but I'm not that smart yet!"
-                    #
-                    #     elif re.match(r'.*who', message_text, re.I):
-                    #         question_message_text = "I know you are asking about someone, but I'm not that smart yet!"
-                    #
-                    #     elif re.match(r".*where|map|wheres|where's", message_text, re.I):
-                    #         question_message_text = "I know you are asking where something is, but I'm not that smart yet!!"
-                    #         # if re.match(r'.*building|library|hall|lab', message_text, re.I):
-                    #         #     pass
-                    #         # elif re.match(r'.*map', message_text, re.I):
-                    #             # send_message(sender_id, "here is a map!\n ")
-                    #
-                    #     else:
-                    #         question_message_text = "I don't even know how you got here"
-                    #     send_message(sender_id, "{}".format(question_message_text))
-                    #
-                    # elif re.match(r'.*reverse|reversed|backwards', message_text, re.I):
-                    #     if len(message_text.split(" ")) > 1:
-                    #         text = message_text.split(" ")[1]
-                    #     else:
-                    #         text = " "
-                    #     send_message(sender_id, "Reversed: {}".format(text[::-1]))
-                    #
-                    # elif re.match(r'.*id', message_text):
-                    #     send_message(sender_id, recipient_id)
-                    #
-                    # else:
-                    #     send_message(sender_id, "idk what you are saying")
-                    #     # send_message(sender_id, "I don't know what you are saying! you said this: {}".format(message_text))
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    if re.match(r'.*hello|hey|hi', message_text, re.I):
+                        send_message(sender_id, "{}, how can I help you today?".format(random.choice(ai_greetings_word_list)))
+
+                    elif re.match(r'.*what|when|date|who|where', message_text, re.I):
+                        if re.match(r'.*what', message_text, re.I):
+                            question_message_text = "I know you are asking a question but I'm not that smart yet! :what"
+                        elif re.match(r'.*when|date', message_text, re.I):
+                            question_message_text = "I know you are asking when something is, but im not that smart yet!"
+                        elif re.match(r'.*who', message_text, re.I):
+                            question_message_text = "I know you are asking about someone, but im not that smart yet!"
+                        elif re.match(r'.*where', message_text, re.I):
+                            question_message_text = "I know you are asking where something is, but I'm not that smart yet!!"
+                        else:
+                            question_message_text = "I dont even know how you got here"
+                        send_message(sender_id, "{}".format(question_message_text))
+
+
+                    elif re.match(r'.*reverse|reversed|backwards', message_text, re.I):
+                        if len(message_text.split(" ")) > 1:
+                            text = message_text.split(" ")[1]
+                        else:
+                            text = " "
+                        send_message(sender_id, "Reversed: {}".format(text[::-1]))
+
+                    else:
+                        send_message(sender_id, "I don't know what you are saying! you said this: {}".format(message_text))
+
+
+
+
+
+
+
 
 
 
@@ -95,16 +95,6 @@ def webhook():
 
 
     return "ok", 200
-
-
-# def times_greeted(times_greeted):
-#     with open("test.csv"):
-#         times_greeted += 1
-#         delimit_file = csv.reader(file, delimiter=",")
-#         times_greeted = []
-#         for line in delimit_file:
-#             times_greeted.append(line)
-
 
 
 def send_message(recipient_id, message_text):
@@ -128,6 +118,16 @@ def send_message(recipient_id, message_text):
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
+
+
+# def question_asking(message_text):
+#     message_text = message_text.split
+#     message_text_length = len(message_text)
+#     while message_text != 0:
+#         if message_text[message_text_length] in ['what', 'whats', "what's", 'when', 'whens', "when's"]:
+#
+#             return message_text
+
 
 
 
