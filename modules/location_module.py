fe32dc081cbdfa9aecb1e3f2912128c998b408ec
jc427727 <dgilb19@gmail.com>
2016-12-01 14:05:16 +1000

    working on putting things in modules, this aint going to work

--- modules/location_module.py
+++ modules/location_module.py
@@ -1,16 +1,20 @@
+process_message_str = "if i see this then its probs working"
 
 
-class LocationPasser:
-    def __init__(self):
-        self.process_message_str = ()
 
-    # def __str__(self):
-    #     return
 
-    def process_message(self):
-        process_message_str = "please let this work"
-
-        return process_message_str
+#
+# class LocationPasser:
+#     def __init__(self):
+#         self.process_message_str = ()
+#
+#     # def __str__(self):
+#     #     return
+#
+#     def process_message(self):
+#         process_message_str = "please let this work"
+#
+#         return process_message_str
 
 
     # def builing_location(self):
