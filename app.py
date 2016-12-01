95735a06ff213595830ee9d656b9f23b7de6a6b7
jc427727 <dgilb19@gmail.com>
2016-12-01 13:31:13 +1000

    working on putting things in modules, i legit dont know

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def webhook():
 
                     elif re.match(r".*map|where|wheres|where's|building|looking|look", message_text, re.I):
 
-                        send_message(sender_id, "{}".format(location_module.LocationPasser()))
+                        send_message(sender_id, "{}".format(location_module.LocationPasser.process_message()))
 
 
 
