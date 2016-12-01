186cd2111e135d9771faef1a15811a2a74034441
jc427727 <dgilb19@gmail.com>
2016-12-01 13:53:27 +1000

    working on putting things in modules, this aint going to work

--- modules/location_module.py
+++ modules/location_module.py
@@ -1,8 +1,14 @@
 
 
 class LocationPasser:
+    def __init__(self):
+        self.process_message_str = "please let this work"
+
+    # def __str__(self):
+    #     return
+
     def process_message(self):
-        return "aegfaegreswgregrgha"
+        return self.process_message_str
 
 
     # def builing_location(self):
