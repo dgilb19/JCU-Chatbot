a432224bc56275ae715892f3be30a8feac61e4e6
jc427727 <dgilb19@gmail.com>
2016-12-02 11:17:26 +1000

    testing!!!!

--- app.py
+++ app.py
@@ -35,14 +35,11 @@ def webhook():
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
     if data["object"] == "page":
-        pass
 
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
-                pass
 
                 if messaging_event.get("message"):  # someone sent us a message
-
 
                     sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
@@ -51,9 +48,6 @@ def webhook():
                     reply = get_reply(message_text)
                     send_message(sender_id, reply)
 
-                    file_save = open('test.csv', 'r')
-                    file_save.write(message_text)
-                    file_save.close()
 
 
                 # if messaging_event.get("delivery"):  # delivery confirmation
