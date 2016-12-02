ce0f820ea1f3403cad7bc6a8ba0c2cb4a8b11cb3
jc427727 <dgilb19@gmail.com>
2016-12-02 10:58:35 +1000

    movign things added more modules added csv test

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def webhook():
             for messaging_event in entry["messaging"]:
                 pass
 
-                if messaging_event.get("message"):  # someone sent us a message
+                for messaging_event.get("message"):  # someone sent us a message
                     pass
 
                     sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
