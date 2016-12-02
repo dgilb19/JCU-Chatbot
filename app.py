28216ea5010ddc510215b1b86c2472c33a92b7a5
jc427727 <dgilb19@gmail.com>
2016-12-02 13:12:58 +1000

    ohh i forgot to remove test, mah bad

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def webhook():
             for messaging_event in entry["messaging"]:
                 if messaging_event.get("message"):  # someone sent us a message
                     opened_file = open('test.csv', 'a')
-                    # opened_file_last_message = open('last_message.csv', 'r')
+                    opened_file_last_message = open('last_message.csv', 'r')
 
                     sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
@@ -54,11 +54,11 @@ def webhook():
                         opened_file.write(message_text + ", ")
                     opened_file.close()
 
-                    # if re.match(r'.*log|logs|history', message_text, re.I):
-                    #     pass
-                    # else:
-                    #     opened_file_last_message.write(message_text)
-                    # opened_file_last_message.close()
+                    if re.match(r'.*log|logs|history', message_text, re.I):
+                        pass
+                    else:
+                        opened_file_last_message.write(message_text)
+                    opened_file_last_message.close()
 
 
 
@@ -112,7 +112,7 @@ def get_reply(message_text):
                 return line
 
     elif re.match(r',*last message', message_text, re.I):
-        with open("test_last_message.csv", "r") as opened_file_last_message:
+        with open("last_message.csv", "r") as opened_file_last_message:
             for line in opened_file_last_message:
                 return line
 
