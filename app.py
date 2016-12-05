1084806650463dd6f512d6c08bd82546aa583e9b
jc427727 <dgilb19@gmail.com>
2016-12-05 10:34:43 +1000

    changed way of getting a list of received messages. testing list_test. doing string handling.... idk how to do this

--- app.py
+++ app.py
@@ -79,12 +79,12 @@ def webhook():
 
 def get_reply(message_text):
     ai_greetings_word_list = ["Hi", "Hello", "Howdy", "Sup my dude"]
-    list_test = ['test 1']
+    list_test = ['test 1', message_text]
 
-    if re.match(r'.*log|logs|history', message_text, re.I):
-        pass
-    else:
-        list_test.append('thing two')
+    # if re.match(r'.*log|logs|history', message_text, re.I):
+    #     pass
+    # else:
+    # list_test.append(message_text)
 
     if re.match(r'.*hello|hey|hi(?!reverse|reversed|backwards)', message_text, re.I):
         return "{}, how can I help you today?".format(random.choice(ai_greetings_word_list))
