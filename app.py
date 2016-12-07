c9eb6a829647900ef6396b6690d5fdb2e326698d
jc427727 <dgilb19@gmail.com>
2016-12-07 15:45:38 +1000

    attempt to fix 2

--- app.py
+++ app.py
@@ -39,7 +39,6 @@ def webhook():
     if data["object"] == "page":
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
-            for messaging_event in entry["messaging"]:
                 if messaging_event.get("message"):  # someone sent us a message
 
                     sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
