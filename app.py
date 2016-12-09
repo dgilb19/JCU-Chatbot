868b46a41db48ddec4f5c4938ee59b9fb8f828e1
jc427727 <dgilb19@gmail.com>
2016-12-09 13:41:27 +1000

    attempt 13 about to give up even though i haven't acheved anything so far

--- app.py
+++ app.py
@@ -85,10 +85,11 @@ def webhook():
 
 
 def get_last_name_used(message_text):
+    last_name_used = []
     with open("peoplelist.csv") as peoplelist:
         for line in peoplelist:
             if message_text in line and len(message_text) >= 3:
-                last_name_used = line.split(", ")[0]
+                last_name_used.append(line.split(", ")[0])
                 return last_name_used
 
 
