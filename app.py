1b99202477dd5ea854be5f94e7aecd5c90daa82f
jc427727 <dgilb19@gmail.com>
2016-12-12 10:45:46 +1000

    attempt 5.9, every time i fix something, something else breaks.....

--- app.py
+++ app.py
@@ -53,8 +53,7 @@ def webhook():
                     #### Testing area
                     if message_text in open("peoplelist.csv") and len(message_text) >= 3:
                         with open("last_name_message.csv" 'w') as last_name:
-
-                            last_name.write("swag")
+                            last_name.write("timmy the bossman")
     # TODO finish this, make it so it saves all names to a csv file
                     # with open("peoplelist.csv") as peoplelist:
                     #     for line in peoplelist:
