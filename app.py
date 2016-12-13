1739f05d619098c7d6bb41da5632bb9a9d5f9298
jc427727 <dgilb19@gmail.com>
2016-12-13 16:43:52 +1000

    6.29 changed something to message text

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ def get_reply(message_text, people_name, building_name):
             return "tjimgugj"
         # TODO fix this ^^^ with the new list of building names i made
         elif re.match(r'.*office|desk', message_text, re.I):
-            location_words.office_passer(people_name)
+            location_words.office_passer(message_text)
             return str(location_words)
         else:
             location_words.location_passer(message_text)
