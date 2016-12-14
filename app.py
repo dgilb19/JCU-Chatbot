83278a3dd6ff90db54025dfd59fddd4cbc273fb1
jc427727 <dgilb19@gmail.com>
2016-12-14 16:03:48 +1000

    11.15 all these lil edit are getting real annoying

--- app.py
+++ app.py
@@ -130,8 +130,9 @@ def get_reply(message_text, people_name, building_name):
     elif re.match(r'.*what', message_text, re.I):
         what_words = WhatIndex(message_text)
         lenght = len(people_name)
-        if people_name[1] in message_text:
-            return str(people_name)
+        for line in people_name:
+            if line in message_text:
+                return str(people_name)
         if re.match(r'.*email', message_text, re.I):
             what_words.email_passer(last_name_message(latest_name=''))
             return str(what_words)
