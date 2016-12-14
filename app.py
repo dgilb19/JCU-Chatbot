887be4620f64ee1fa70275a771318ce48c680d69
jc427727 <dgilb19@gmail.com>
2016-12-14 13:39:56 +1000

    6.11 printing thigns

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def webhook():
                     with open("classlist.csv") as class_name_list:
                         for line in class_name_list:
                             line_one = line.split(", ")[3]
-                            line_two = line.split(', ')[5]
+                            line_two = line.split(', ')[1]
 
                             class_name += tuple(line_one.split(", "))
 
