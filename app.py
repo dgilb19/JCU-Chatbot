91f8f2990fd140dbc7b0b29100956260607a27e9
jc427727 <dgilb19@gmail.com>
2016-12-15 09:07:52 +1000

    2

--- app.py
+++ app.py
@@ -123,132 +123,135 @@ def last_building_message(latest_building):
 
 def get_reply(message_text, people_name, building_name):
     ai_greetings_word_list = ["Hi", "Hello", "Howdy", "Sup my dude"]
+    if re.match(r'remove', message_text, re.I):
+        print(message_text[1:])
+        print(message_text.split(" ")[1:])
 
-    if re.match(r'.*hello|hey|hi|yo(?!reverse|reversed|backwards)', message_text, re.I):
-        return "{}, how can I help you today?".format(random.choice(ai_greetings_word_list))
+        if re.match(r'.*hello|hey|hi|yo(?!reverse|reversed|backwards)', message_text, re.I):
+            return "{}, how can I help you today?".format(random.choice(ai_greetings_word_list))
 
-    elif re.match(r'.*what', message_text, re.I):
-        what_words = WhatIndex(message_text)
-        if re.match(r'.*email', message_text, re.I):
-            for line in people_name:
-                if line in message_text and message_text >= 3:
-                    # return "test {}".format(line)
-                    what_words.email_passer(last_name_message(latest_name=''))
-                    return str(what_words)
+        elif re.match(r'.*what', message_text, re.I):
+            what_words = WhatIndex(message_text)
+            if re.match(r'.*email', message_text, re.I):
+                for line in people_name:
+                    if line in message_text and message_text >= 3:
+                        # return "test {}".format(line)
+                        what_words.email_passer(last_name_message(latest_name=''))
+                        return str(what_words)
+                else:
+                    return "idk who you are talking about"
+
+    # TODO fix this
+
             else:
-                return "idk who you are talking about"
-
-# TODO fix this
-
-        else:
-            return "I know you are asking a question but I'm not that smart yet! :what"
+                return "I know you are asking a question but I'm not that smart yet! :what"
 
 
-#### TODO: make this work
-    elif re.match(r'.*when|whens|date|exam|exams', message_text, re.I):
-        date_words = DateIndex(message_text)
-        if message_text in open("datelist.csv").read():
-            date_words.date_passer(message_text)
-            return str(date_words)
-        elif re.match(r'.*class|lecture|practical|prac', message_text, re.I):
-            if last_name_message == "":
-                return "Who are you talking about?"
-            else:
-                date_words.class_list_passer(message_text)
+    #### TODO: make this work
+        elif re.match(r'.*when|whens|date|exam|exams', message_text, re.I):
+            date_words = DateIndex(message_text)
+            if message_text in open("datelist.csv").read():
+                date_words.date_passer(message_text)
                 return str(date_words)
-            # TODO make this work
-        elif re.match(r'.*exam', message_text, re.I):
+            elif re.match(r'.*class|lecture|practical|prac', message_text, re.I):
+                if last_name_message == "":
+                    return "Who are you talking about?"
+                else:
+                    date_words.class_list_passer(message_text)
+                    return str(date_words)
+                # TODO make this work
+            elif re.match(r'.*exam', message_text, re.I):
+                date_words.exam_list_passer(message_text)
+                return str(date_words)
+            else:
+                date_words.date_passer(message_text)
+                return str(date_words)
+    ####
+
+        elif message_text >= 5 and message_text in open("examlist.csv").read():
+            date_words = DateIndex(message_text)
             date_words.exam_list_passer(message_text)
             return str(date_words)
-        else:
+
+        elif message_text >= 5 and message_text in open("datelist.csv").read():
+            date_words = DateIndex(message_text)
             date_words.date_passer(message_text)
             return str(date_words)
-####
 
-    elif message_text >= 5 and message_text in open("examlist.csv").read():
-        date_words = DateIndex(message_text)
-        date_words.exam_list_passer(message_text)
-        return str(date_words)
-
-    elif message_text >= 5 and message_text in open("datelist.csv").read():
-        date_words = DateIndex(message_text)
-        date_words.date_passer(message_text)
-        return str(date_words)
-
-    elif re.match(r".*who|whos|who's", message_text, re.I):
-        if message_text in open("peoplelist.csv").read():
-            people_words = PeopleIndex(message_text)
-            people_words.people_passer(message_text)
-            return str(people_words)
-        else:
-            who_words = PeopleIndex(message_text)
-            who_words.change_words_to_jerry(message_text)
-            return str(who_words)
-
-    elif len(message_text) >= 3 and message_text in open("peoplelist.csv").read():
-        with open("peoplelist.csv") as peoplelist:
-            for line in peoplelist:
-                if message_text in line:
-                    return "What about {}?".format(line.title().split(", ")[0])
-
-    elif re.match(r".*map|where|wheres|where's|building|looking|look [0-354]", message_text, re.I):
-        location_words = LocationIndex(message_text)
-        if message_text in building_name:
-            location_words.location_name_passer(message_text)
-            return "tjimgugj"
-        # TODO fix this ^^^ with the new list of building names i made
-        elif re.match(r'.*office|desk', message_text, re.I):
-            location_words.office_passer(last_name_message(latest_name=''))
-            return str(location_words)
-        else:
-            location_words.location_passer(message_text)
-            return str(location_words)
-
-    elif any(message_text.find(s) >= 0 for s in building_name):
-        location_words = LocationIndex(message_text)
-        location_words.location_name_passer(message_text)
-        return str(location_words)
-
-    elif re.match(r'.*reverse|reversed|backwards', message_text, re.I):
-        if len(message_text.split(" ")) > 1:
-            if re.match(r".*reverse", message_text, re.I):
-                text = message_text[8:]
+        elif re.match(r".*who|whos|who's", message_text, re.I):
+            if message_text in open("peoplelist.csv").read():
+                people_words = PeopleIndex(message_text)
+                people_words.people_passer(message_text)
+                return str(people_words)
             else:
-                text = message_text[9:]
-        else:
-            text = " "
-        return "Reversed: {}".format(text[::-1])
-    elif re.match(r'timetable|schedule|class|practical|prac|show', message_text, re.I):
-        # timetable_words = class_list_passer
-        if re.match(r'.*show', message_text, re.I):
-            return 'http://i.imgur.com/wKBDSfM.png'
-        # elif re.match(r'.*', message_text, re,I):
-        # elif re.match(r".*"):
-        #     return "10 out of 10, good job"
-        else:
-            return "what about your schedule?"
+                who_words = PeopleIndex(message_text)
+                who_words.change_words_to_jerry(message_text)
+                return str(who_words)
 
-    elif re.match(r'.*help', message_text, re.I):
-        return "Ask me where a certain building is, ask for a map, or about someone(im not a very good bot so i only " \
-               "know a few people(try Daniel)) "
+        elif len(message_text) >= 3 and message_text in open("peoplelist.csv").read():
+            with open("peoplelist.csv") as peoplelist:
+                for line in peoplelist:
+                    if message_text in line:
+                        return "What about {}?".format(line.title().split(", ")[0])
 
-    elif re.match(r'.*allan|cameron|cam|sanio|jesse|ramisa|remi|bangarang', message_text, re.I):
-        if re.match(r'.*allan', message_text, re.I):
-            return "Allan you are a drongo"
-        elif re.match(r".*cameron|cam", message_text, re.I):
-            return "Rena loves yogurt"
-        elif re.match(r".*sanio", message_text, re.I):
-            return "Sorry"
-        elif re.match(r".*jesse", message_text, re.I):
-            return "heres Jesse's number; 0412263945"
-        elif re.match(r'.*bangarang', message_text, re.I):
-            return "Allan stop, thats not a word, its a skrillex song"
+        elif re.match(r".*map|where|wheres|where's|building|looking|look [0-354]", message_text, re.I):
+            location_words = LocationIndex(message_text)
+            if message_text in building_name:
+                location_words.location_name_passer(message_text)
+                return "tjimgugj"
+            # TODO fix this ^^^ with the new list of building names i made
+            elif re.match(r'.*office|desk', message_text, re.I):
+                location_words.office_passer(last_name_message(latest_name=''))
+                return str(location_words)
+            else:
+                location_words.location_passer(message_text)
+                return str(location_words)
+
+        elif any(message_text.find(s) >= 0 for s in building_name):
+            location_words = LocationIndex(message_text)
+            location_words.location_name_passer(message_text)
+            return str(location_words)
+
+        elif re.match(r'.*reverse|reversed|backwards', message_text, re.I):
+            if len(message_text.split(" ")) > 1:
+                if re.match(r".*reverse", message_text, re.I):
+                    text = message_text[8:]
+                else:
+                    text = message_text[9:]
+            else:
+                text = " "
+            return "Reversed: {}".format(text[::-1])
+        elif re.match(r'timetable|schedule|class|practical|prac', message_text, re.I):
+            # timetable_words = class_list_passer
+            if re.match(r'.*show', message_text, re.I):
+                return 'http://i.imgur.com/wKBDSfM.png'
+            # elif re.match(r'.*', message_text, re,I):
+            # elif re.match(r".*"):
+            #     return "10 out of 10, good job"
+            else:
+                return "what about your schedule?"
+
+        elif re.match(r'.*help', message_text, re.I):
+            return "Ask me where a certain building is, ask for a map, or about someone(im not a very good bot so i only " \
+                   "know a few people(try Daniel)) "
+
+        elif re.match(r'.*allan|cameron|cam|sanio|jesse|ramisa|remi|bangarang', message_text, re.I):
+            if re.match(r'.*allan', message_text, re.I):
+                return "Allan you are a drongo"
+            elif re.match(r".*cameron|cam", message_text, re.I):
+                return "Rena loves yogurt"
+            elif re.match(r".*sanio", message_text, re.I):
+                return "Sorry"
+            elif re.match(r".*jesse", message_text, re.I):
+                return "heres Jesse's number; 0412263945"
+            elif re.match(r'.*bangarang', message_text, re.I):
+                return "Allan stop, thats not a word, its a skrillex song"
+            else:
+                return "ａｅｓｔｈｅｔｉｃ"
+
         else:
-            return "ａｅｓｔｈｅｔｉｃ"
-
-    else:
-        return "idk what you are saying"
-        # send_message(sender_id, "I don't know what you are saying! you said this: {}".format(message_text))
+            return "idk what you are saying"
+            # send_message(sender_id, "I don't know what you are saying! you said this: {}".format(message_text))
 
 
 def send_message(recipient_id, message_text):
