f83da3585e3ef40fb44f91ec6b695ecfc9847f0b
jc427727 <dgilb19@gmail.com>
2016-12-16 09:47:45 +1000

    1.11 it did.....

--- modules/location_module.py
+++ modules/location_module.py
@@ -71,8 +71,8 @@ class LocationIndex:
                         self.location_str = "{} does not have an office".format(line.split(", ")[0].title())
                     else:
                         line = line.split(", ")[2]
-                        # self.location_str = "{} : {}".format(line.split("-")[0], line.split("-")[1])
-                        self.location_str = line
+                        self.location_str = "{} : {}".format(line.split("-")[0], line.split("-")[1])
+                        # self.location_str = line
 
 # TODO add more keywords and create a csv with building and there numbers so it can tell the user the name/ they can
 # put the name in and still get the map up
