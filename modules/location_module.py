c4d384053c8559ac79dbca567d2ceb233c202453
jc427727 <dgilb19@gmail.com>
2016-12-16 09:13:14 +1000

    ok i think i may have figured out what the problem was the day of yester

--- modules/location_module.py
+++ modules/location_module.py
@@ -65,9 +65,9 @@ class LocationIndex:
         with open("peoplelist.csv") as peoplelist:
             for line in peoplelist:
                 if name in line:
-                    # line = line.split(", ")[2]
-                    # self.location_str = "{} : {}".format(line.split("-")[0], line.split("-")[1])
-                    self.location_str = line.split(', ')[2]
+                    line = line.split(", ")[2]
+                    self.location_str = "{} : {}".format(line.split("-")[0], line.split("-")[1])
+                    # self.location_str = line.split(', ')[2]
 
 # TODO add more keywords and create a csv with building and there numbers so it can tell the user the name/ they can
 # put the name in and still get the map up
