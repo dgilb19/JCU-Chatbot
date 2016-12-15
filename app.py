dcb97b5c19ac56ced15f288d481e1db007a4d74c
jc427727 <dgilb19@gmail.com>
2016-12-15 15:28:49 +1000

    14.2 im not sure whats going on

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ def get_reply(message_text, people_name, building_name):
                 what_words.email_passer(last_name_message(latest_name=''))
                 return str(what_words)
 
-        elif re.match(r'happening|going on|today|', message_text, re.I):
+        elif re.match(r'.*happening', message_text, re.I):
             return "there is nothing going on today, go home"
 
         else:
