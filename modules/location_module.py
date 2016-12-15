4e0c98b62d12745529f2745814811e9e8ac515a6
jc427727 <dgilb19@gmail.com>
2016-12-16 09:49:14 +1000

    2

--- modules/location_module.py
+++ modules/location_module.py
@@ -71,7 +71,7 @@ class LocationIndex:
                         self.location_str = "{} does not have an office".format(line.split(", ")[0].title())
                     else:
                         line = line.split(", ")[2]
-                        self.location_str = "{} : {}".format(line.split("-")[0], line.split("-")[1])
+                        self.location_str = "their office is in building {}, room {}".format(line.split("-")[0], line.split("-")[1])
                         # self.location_str = line
 
 # TODO add more keywords and create a csv with building and there numbers so it can tell the user the name/ they can
