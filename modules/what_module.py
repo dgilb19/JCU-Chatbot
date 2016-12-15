87a96a60682856aa9b5dd34a010951e4e81ba257
jc427727 <dgilb19@gmail.com>
2016-12-15 12:01:52 +1000

    8.18 almost? whyyy??

--- modules/what_module.py
+++ modules/what_module.py
@@ -21,3 +21,5 @@ class WhatIndex:
                 if name in line:
                     self.what_str = "???"
                     # self.what_str = line.split(", ")[1]
+                else:
+                    self.what_str = "this would make me even more confused"
