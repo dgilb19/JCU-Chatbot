e03076ff922f0188a12df9e6b39de0ad07598e17
jc427727 <dgilb19@gmail.com>
2016-12-16 15:04:39 +1000

    15.1 making it better addding '?' to it

--- app.py
+++ app.py
@@ -48,13 +48,18 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    if "?" in message_text:
-                        message_text = message_text[:-1]
-                    else:
-                        pass
-                    # if "'" in message_text:
+                    # if "?" in message_text:
+                    #     message_text = message_text[:-1]
+                    # else:
+                    #     pass
+                    word = ""
                     for letter in message_text:
-                        print letter
+                        if letter == "'" or "?":
+                            pass
+                        else:
+                            word += letter
+                    message_text = word
+                    print message_text
 
 
                     with open("peoplelist.csv") as peoplelist:
