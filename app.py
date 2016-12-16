887f14a681f41e5ea9deee838f787fd86376b36d
jc427727 <dgilb19@gmail.com>
2016-12-16 11:25:20 +1000

    4.22 striping \n

--- app.py
+++ app.py
@@ -218,6 +218,7 @@ def get_reply(message_text, people_name, building_name):
         with open("buildinglist.csv") as buildinglist:
             for line in buildinglist:
                 line = line.split(", ")[1]
+                line = line.strip("\n")
                 print line
                 if message_text in line:
 
